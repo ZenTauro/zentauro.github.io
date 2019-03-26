87720442307f8dd53afa661c203a3b3acd7cdf6c
zentauro <pedrogomezmartin1999@gmail.com>
2019-03-26 11:49:03 +0100

    better style

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f1a5c631df0c9f65a765aff9f277df70.js"
+  "/precache-manifest.5cf7fe88da3ad60c209087f92fa50edb.js"
 );
 
 workbox.clientsClaim();
