2bd217e500208d6784b26b5b436e51e096e5335e
zentauro <pedrogomezmartin1999@gmail.com>
2019-03-26 10:39:00 +0100

    mejoras

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.50e6eabeb52867f467f304c65a00abaf.js"
+  "/precache-manifest.080c4e10bfa96fb2370278ebdc4d6a12.js"
 );
 
 workbox.clientsClaim();
