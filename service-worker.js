0b35dedbae6be3adbd5c11ab39277fabf8093a83
zentauro <pedrogomezmartin1999@gmail.com>
2019-03-28 18:56:51 +0100

    arreglado

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.01385b796257b60078b1193b3255766d.js"
+  "/precache-manifest.c8d059c3e245becc682d38385d622096.js"
 );
 
 workbox.clientsClaim();
